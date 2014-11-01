8d5dddd587fd5caa957d03c3403ddd56e6eb8d59
Thong B. Tran <baocongchen@yahoo.com>
2014-11-01 13:03:06 +0900

    Update Week_1_practice.py

--- Week_1_practice.py
+++ Week_1_practice.py
@@ -14,13 +14,6 @@ def stochasticNumber():
     '''
     return random.randrange(10,22,2)
 ###########################    
-import string
-PATH_TO_FILE = 'words.txt'
-def loadWords():
-	inFile = open(PATH_TO_FILE, 'r', 0)
-	line = inFile.readline()
-	wordlist = string.split(line,',')
-	print "  ", len(wordlist), "words loaded."
-	return wordlist
+
 ############################
 
