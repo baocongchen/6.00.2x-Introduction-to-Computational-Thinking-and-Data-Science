69f0ed501143e0b44d72b71093353eacdd045dd4
Thong B. Tran <baocongchen@yahoo.com>
2014-11-01 13:02:46 +0900

    Update Week_1_practice.py

--- Week_1_practice.py
+++ Week_1_practice.py
@@ -4,5 +4,23 @@ def genEven():
     Returns a random even number x, where 0 <= x < 100
     '''
     return random.randrange(0,100,2)
+###########################    
 def deterministicNumber():
     return 10 # or 12 or 14 or 16 or 18 or 20
+###########################
+def stochasticNumber():
+    '''
+    Stochastically generates and returns a uniformly distributed even number between 9 and 21
+    '''
+    return random.randrange(10,22,2)
+###########################    
+import string
+PATH_TO_FILE = 'words.txt'
+def loadWords():
+	inFile = open(PATH_TO_FILE, 'r', 0)
+	line = inFile.readline()
+	wordlist = string.split(line,',')
+	print "  ", len(wordlist), "words loaded."
+	return wordlist
+############################
+
