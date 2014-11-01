749514b8c68a7cd39d740177d23b86fc22f704fc
Thong B. Tran <baocongchen@yahoo.com>
2014-11-01 11:34:01 +0900

    Update Week_1_practice.py

--- Week_1_practice.py
+++ Week_1_practice.py
@@ -4,3 +4,5 @@ def genEven():
     Returns a random even number x, where 0 <= x < 100
     '''
     return random.randrange(0,100,2)
+def deterministicNumber():
+    return 10 # or 12 or 14 or 16 or 18 or 20
