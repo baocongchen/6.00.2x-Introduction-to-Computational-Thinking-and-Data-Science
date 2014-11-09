3a7e1f335d91020657b1b0480693befc265ccd08
Thong B. Tran <baocongchen@yahoo.com>
2014-11-09 14:31:39 +0900

    Update Week_2_practice.py

--- Week_2_practice.py
+++ Week_2_practice.py
@@ -107,3 +107,47 @@ def stdDevOfLengths(L):
             total = total + (i - average)**2       
         stdDev = (total/float(len(L)))**0.5
         return stdDev
+
+
+#Practice 2
+import pylab
+
+# You may have to change this path
+WORDLIST_FILENAME = "words.txt"
+
+def loadWords():
+    """
+    Returns a list of valid words. Words are strings of uppercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    # wordList: list of strings
+    wordList = []
+    for line in inFile:
+        wordList.append(line.strip().lower())
+    print "  ", len(wordList), "words loaded."
+    return wordList
+
+def plotVowelProportionHistogram(wordList, numBins=15):
+    """
+    Plots a histogram of the proportion of vowels in each word in wordList
+    using the specified number of bins in numBins
+    """
+    ratios = []
+    vowels = "aeiouAEIOU"
+    for word in wordList:
+        count = 0
+        for letter in word:
+            if letter in vowels:
+                count += 1
+        ratios.append(count/float(len(word)))
+    pylab.hist(ratios,numBins)
+    
+    pylab.show()
+if __name__ == '__main__':
+    wordList = loadWords()
+    plotVowelProportionHistogram(wordList,)
