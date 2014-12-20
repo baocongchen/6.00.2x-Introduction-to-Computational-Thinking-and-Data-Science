e3c13db6f58677454d1723b5faa4fffd1e8d2a62
baocongchen <baocongchen@yahoo.com>
2014-12-20 19:04:42 +0900

    practice and on-video questions

---
+++ Week_5_practice.py
@@ -0,0 +1,302 @@
+#Session 1
+import pylab
+
+class Item(object):
+    def __init__(self, n, v, w):
+        self.name = n
+        self.value = float(v)
+        self.weight = float(w)
+    def getName(self):
+        return self.name
+    def getValue(self):
+        return self.value
+    def getWeight(self):
+        return self.weight
+    def __str__(self):
+        result = '<' + self.name + ', ' + str(self.value) + ', '\
+                 + str(self.weight) + '>'
+        return result
+
+def buildItems():
+    names = ['clock', 'painting', 'radio', 'vase', 'book',
+             'computer']
+    vals = [175,90,20,50,10,200]
+    weights = [10,9,4,2,1,20]
+    Items = []
+    for i in range(len(vals)):
+        Items.append(Item(names[i], vals[i], weights[i]))
+    return Items
+
+def greedy(Items, maxWeight, keyFcn):
+    assert type(Items) == list and maxWeight >= 0
+    ItemsCopy = sorted(Items, key=keyFcn, reverse = True)
+    result = []
+    totalVal = 0.0
+    totalWeight = 0.0
+    i = 0
+    while totalWeight < maxWeight and i < len(Items):
+        if (totalWeight + ItemsCopy[i].getWeight()) <= maxWeight:
+            result.append((ItemsCopy[i]))
+            totalWeight += ItemsCopy[i].getWeight()
+            totalVal += ItemsCopy[i].getValue()
+        i += 1
+    return (result, totalVal)
+
+def value(item):
+    return item.getValue()
+
+def weightInverse(item):
+    return 1.0/item.getWeight()
+
+def density(item):
+    return item.getValue()/item.getWeight()
+
+def testGreedy(Items, constraint, getKey):
+    taken, val = greedy(Items, constraint, getKey)
+    print ('Total value of items taken = ' + str(val))
+    for item in taken:
+        print '  ', item
+
+def testGreedys(maxWeight = 20):
+    Items = buildItems()
+    print('Items to choose from:')
+    for item in Items:
+        print '  ', item
+    print 'Use greedy by value to fill a knapsack of size', maxWeight
+    testGreedy(Items, maxWeight, value)
+    print 'Use greedy by weight to fill a knapsack of size', maxWeight
+    testGreedy(Items, maxWeight, weightInverse)
+    print 'Use greedy by density to fill a knapsack of size', maxWeight
+    testGreedy(Items, maxWeight, density)
+
+def dToB(n, numDigits):
+    """requires: n is a natural number less than 2**numDigits
+      returns a binary string of length numDigits representing the
+              the decimal number n."""
+    assert type(n)==int and type(numDigits)==int and n >=0 and n < 2**numDigits
+    bStr = ''
+    while n > 0:
+        bStr = str(n % 2) + bStr
+        n = n//2
+    while numDigits - len(bStr) > 0:
+        bStr = '0' + bStr
+    return bStr
+
+def genPset(Items):
+    """Generate a list of lists representing the power set of Items"""
+    numSubsets = 2**len(Items)
+    templates = []
+    for i in range(numSubsets):
+        templates.append(dToB(i, len(Items)))
+    pset = []
+    for t in templates:
+        elem = []
+        for j in range(len(t)):
+            if t[j] == '1':
+                elem.append(Items[j])
+        pset.append(elem)
+    return pset
+
+def chooseBest(pset, constraint, getVal, getWeight):
+    bestVal = 0.0
+    bestSet = None
+    for Items in pset:
+        ItemsVal = 0.0
+        ItemsWeight = 0.0
+        for item in Items:
+            ItemsVal += getVal(item)
+            ItemsWeight += getWeight(item)
+        if ItemsWeight <= constraint and ItemsVal > bestVal:
+            bestVal = ItemsVal
+            bestSet = Items
+    return (bestSet, bestVal)
+
+def testBest():
+    Items = buildItems()
+    pset = genPset(Items)
+    taken, val = chooseBest(pset, 20, Item.getValue, Item.getWeight)
+    print ('Total value of items taken = ' + str(val))
+    for item in taken:
+        print '  ', item
+
+def maxVal(toConsider, avail): 
+    if toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0].getWeight() > avail:
+        result = maxVal(toConsider[1:], avail)
+    else:
+        nextItem = toConsider[0]
+
+        #Explore left branch
+        withVal, withToTake = maxVal(toConsider[1:],
+                                      avail - nextItem.getWeight())
+        withVal += nextItem.getValue()
+        #Explore right branch
+        withoutVal, withoutToTake = maxVal(toConsider[1:], avail)
+
+        #Choose better branch
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+    return result
+
+def smallTest():
+    Items = buildItems()
+    val, taken = maxVal(Items, 20)
+    for item in taken:
+        print(item)
+    print ('Total value of items taken = ' + str(val))
+
+
+
+#Part 1
+NUMBER = 3
+def look_for_things(myList):
+    """Looks at all elements"""
+    for n in myList:
+        if n == NUMBER:
+            return True
+    return False
+# the big-O upper bound runtime for the function look_for_things is O(n)
+
+
+#Part 2
+NUMBER = 3
+def look_for_other_things(myList):
+    """Looks at all pairs of elements"""
+    for n in myList:
+        for m in myList:
+            if n - m == NUMBER or m - n == NUMBER:
+                return True
+    return False
+# the big-O upper bound runtime for the function look_for_things is O(n^2)
+
+
+#Part 3
+def get_all_subsets(some_list):
+    """Returns all subsets of size 0 - len(some_list) for some_list"""
+    if len(some_list) == 0:
+        # If the list is empty, return the empty list
+        return [[]]
+    subsets = []
+    first_elt = some_list[0]
+    rest_list = some_list[1:]
+    # Strategy: Get all the subsets of rest_list; for each
+    # of those subsets, a full subset list will contain both
+    # the original subset as well as a version of the subset
+    # that contains first_elt
+    for partial_subset in get_all_subsets(rest_list):
+        subsets.append(partial_subset)
+        next_subset = partial_subset[:] + [first_elt]
+        subsets.append(next_subset)
+    return subsets
+
+NUMBER = 3
+def look_for_all_the_things(myList):
+    """Looks at all subsets of this list"""
+    # Make subsets
+    all_subsets = get_all_subsets(myList)
+    for subset in all_subsets:
+        if sum(subset) == NUMBER:
+            return True
+    return False
+# the big-O upper bound runtime for the function look_for_things is O(2^n)
+
+
+
+#Session 2
+
+class Node(object):
+    def __init__(self, name):
+        self.name = str(name)
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge(object):
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+    def getSource(self):
+        return self.src
+    def getDestination(self):
+        return self.dest
+    def __str__(self):
+        return str(self.src) + '->' + str(self.dest)
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, weight = 1.0):
+        self.src = src
+        self.dest = dest
+        self.weight = weight
+    def getWeight(self):
+        return self.weight
+    def __str__(self):
+        return str(self.src) + '->(' + str(self.weight) + ')'\
+            + str(self.dest)
+
+class Digraph(object):
+    def __init__(self):
+        self.nodes = set([])
+        self.edges = {}
+    def addNode(self, node):
+        if node in self.nodes:
+            raise ValueError('Duplicate node')
+        else:
+            self.nodes.add(node)
+            self.edges[node] = []
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not(src in self.nodes and dest in self.nodes):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+    def childrenOf(self, node):
+        return self.edges[node]
+    def hasNode(self, node):
+        return node in self.nodes
+    def __str__(self):
+        res = ''
+        for k in self.edges:
+            for d in self.edges[k]:
+                res = res + str(k) + '->' + str(d) + '\n'
+        return res[:-1]
+
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+
+def printPath(path):
+    # a path is a list of nodes
+    result = ''
+    for i in range(len(path)):
+        if i == len(path) - 1:
+            result = result + str(path[i])
+        else:
+            result = result + str(path[i]) + '->'
+    return result
+
+#Part 1
+nodes = []
+nodes.append(Node("ABC")) # nodes[0]
+nodes.append(Node("ACB")) # nodes[1]
+nodes.append(Node("BAC")) # nodes[2]
+nodes.append(Node("BCA")) # nodes[3]
+nodes.append(Node("CAB")) # nodes[4]
+nodes.append(Node("CBA")) # nodes[5]
+
+g = Graph()
+for n in nodes:
+    g.addNode(n)
+
+g.addEdge(Edge(nodes[0], nodes[1]))
+g.addEdge(Edge(nodes[0], nodes[2]))
+g.addEdge(Edge(nodes[1], nodes[4]))
+g.addEdge(Edge(nodes[2], nodes[3]))
+g.addEdge(Edge(nodes[3], nodes[5]))
+g.addEdge(Edge(nodes[4], nodes[5]))
